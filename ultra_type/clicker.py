902bda2acf1a33c9f2ddea17953aa76923e6b080
eran.heres@gmail.com <eran.heres@gmail.com>
2023-12-10 14:48:02 +0200

    don't use click if env does not support

--- ultra_type/clicker.py
+++ ultra_type/clicker.py
@@ -1,15 +1,21 @@
 import pygame
 import random
 
+
 class Clicker:
 
     def __init__(self):
         # Initialize Pygame Mixer
-        pygame.mixer.init()
-        # Load the click sound
-        self._click_sound = pygame.mixer.Sound('ultra_type/data/click-2.wav')
+        try:
+            pygame.mixer.init()
+            # Load the click sound
+            self._click_sound = pygame.mixer.Sound('ultra_type/data/click-2.wav')
+        except Exception as e:
+            self._click_sound = None
 
     def click(self):
+        if self._click_sound is None:
+            return
         try:
             self._click_sound.set_volume(random.random() * 0.5 + 0.5)
             #self._click_sound.play()
